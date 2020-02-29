efdc41d2d41ee5923cb8f8792423696dd9d36e0a
陶翔 <thawsoar@gmail.com>
2020-02-29 23:15:51 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App with docker"/>
+    <HelloWorld msg="Welcome to my website with docker"/>
   </div>
 </template>
 
